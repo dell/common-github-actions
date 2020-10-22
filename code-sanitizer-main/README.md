b4b61a82bce8701dd5e25f554d3cca3ef7a06d4c
Ameer Jabbar <ameer.jabbar@dell.com>
2020-10-21 20:13:40 -0400

    Initial commit

---
+++ code-sanitizer-main/README.md
@@ -0,0 +1,31 @@
+# Code Sanitizer GitHub Action
+This GitHub Action can be used to check sources files for forbidden words and text. Forbidden words include 
+such things as non-inclusive language, non-private IP addresses. 
+
+To enable this Action, you can create a .yml file under your repo's .github/workflows directory. 
+Simple example:
+
+```
+name: Sanitize source
+
+on:
+  push:
+    branches: [ master ]
+  pull_request:
+    branches: [ master ]
+
+jobs:
+
+  build:
+    name: Check for forbidden words
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout the code
+        uses: actions/checkout@v2
+      - name: Run the forbidden words scan
+        uses: dell/code-sanitizer@main
+        with:
+          args: .
+```
+
+The `args` for the Action is a path in which to search for forbidden words. You can use '.' to search from the root of the repo.
---
+++ code-sanitizer-main/entrypoint.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DIRECTORY=`pwd`
+TOTALFOUND=0
+SCRIPTNAME=$(basename "$0")
+OUTPUT=""
+
+cd "${SCRIPTDIR}"
+for file_name in "${SCRIPTDIR}"/*.forbidden-word-list; do
+   source "${file_name}"
+done
+
+function usage {
+   echo
+   echo "`basename ${0}`"
+   echo "    -d directory        - Directory to scan, default is the current working directory"
+   echo "    -t output filename  - Write results to a file and stdout"
+   exit 1
+}
+
+# output -
+function output {
+  if [ "${OUTPUT}blank" == "blank" ]; then
+    grep -E -iwRHI --color --line-number --exclude-dir .git --exclude-dir .github --exclude-dir .idea --exclude "*.forbidden-word-list" "${W}" .
+  else
+    grep -E -iwRHI --line-number --exclude-dir .git --exclude-dir .github --exclude-dir .idea --exclude "*.forbidden-word-list" "${W}" . | tee -a ${OUTPUT}
+  fi
+}
+
+# check_for_disallowed_words
+# $1 = working dir
+function check_for_disallowed_words {
+  cd "${DIRECTORY}"
+  for A in "${DISALLOWEDWORDS[@]}"; do
+    IFS='|' tokens=( "$A" )
+    W="${tokens[0]}"
+    T="${tokens[1]}"
+    E="${tokens[2]}"
+
+    echo
+
+    echo "-- Checking for ${T}, with a regex of '${W}' because ${E}"
+
+    COUNT=$(grep -E -iwRHI --color --line-number --exclude-dir .git --exclude-dir .github --exclude-dir .idea --exclude "*.forbidden-word-list" "${W}" . | wc -l)
+    TOTALFOUND=$((TOTALFOUND + COUNT))
+
+    if [ "${COUNT}" != "0" ]; then
+      output
+      echo
+    fi
+  done
+
+}
+
+while getopts "t:d:h" opt; do
+  case $opt in
+    d)
+      DIRECTORY="${OPTARG}"
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    t)
+      OUTPUT="${OPTARG}"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ "${OUTPUT}blank" != "blank" ]; then
+  rm -f "${OUTPUT}"
+fi
+
+check_for_disallowed_words "${BASEDIR}/${SOURCE_CLONE}"
+echo "Total issues found: $TOTALFOUND"
+exit $TOTALFOUND
