6d7399016c7f51b7a28070f22560080e0073d5e0
root <shayna_finocchiaro@dell.com>
2025-02-06 19:28:24 +0000

    debug

--- go-code-tester/entrypoint.sh
+++ go-code-tester/entrypoint.sh
@@ -133,6 +133,7 @@ for submodule in $submodules; do
 
     coverage_results["$package"]=$coverage
 
+    cat cover.out
     # Append coverage results to combined file for coverage report
     cat cover.out >> coverage.txt > /dev/null
   done
