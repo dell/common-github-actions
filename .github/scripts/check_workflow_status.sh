3c5a82d444d9150696405207f242f7a873ba8970
khareRajshree <rajshree.khare@dell.com>
2025-02-10 19:43:16 +0530

    add trigger job for releasing golibs

--- .github/scripts/check_workflow_status.sh
+++ .github/scripts/check_workflow_status.sh
@@ -1,3 +1,13 @@
+# Copyright (c) 2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+
+# Script to check the status of the latest workflow run for a specified event type
+
 #!/bin/bash
 
 GITHUB_TOKEN=$1
