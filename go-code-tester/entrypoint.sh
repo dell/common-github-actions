bd9b57a308c1f6b262bf2ebf126959fd3516b68f
shaynafinocchiaro <shayna_finocchiaro@dell.com>
2025-04-09 13:41:14 -0400

    Update go-code-tester for excluded directories (#147)
    * don't exit from script
    
    * add exclusion when finding subdirectories
    
    * support for multiple excluded directories
    
    * log submodules
    
    * revert to main branch

--- go-code-tester/entrypoint.sh
+++ go-code-tester/entrypoint.sh
@@ -75,7 +75,12 @@ check_coverage() {
 }
 
 # Find all directories containing go.mod files
-submodules=$(find . -name 'go.mod' -exec dirname {} \;)
+if [ -n "$EXCLUDE_DIRECTORIES" ]; then
+    EXCLUDED_PATHS=$(echo "$EXCLUDE_DIRECTORIES" | sed 's/|/\/\*\" -not -path \"/g' | sed 's/^/-not -path \"/' | sed 's/$/\/\*\"/')
+    submodules=$(eval find . -name 'go.mod' "$EXCLUDED_PATHS" -exec dirname {} +)
+else
+    submodules=$(find . -name 'go.mod' -exec dirname {} +)
+fi
 
 # Submodules may not exist if testing in a specific TEST_FOLDER
 if [[ -z "$submodules" ]]; then
