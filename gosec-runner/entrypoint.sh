5a3b54af9503c8d4a83d1f03902a8d64710904ef
Fernando Alfaro Campos <Fernando.Alfaro_campos@Dell.com>
2025-04-17 13:34:57 -0400

    Iterate through all directories with go.mod files and run gosec

--- gosec-runner/entrypoint.sh
+++ gosec-runner/entrypoint.sh
@@ -26,14 +26,31 @@ fi
 LATEST_VERSION=$(curl -s https://api.github.com/repos/securego/gosec/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $LATEST_VERSION
-echo "run gosec command: $(go env GOPATH)/bin/gosec -exclude-generated $EXCLUDE_FLAG $EXCLUDE_DIR_FLAG $DIRECTORIES"
-$(go env GOPATH)/bin/gosec -exclude-generated $EXCLUDE_FLAG $EXCLUDE_DIR_FLAG $DIRECTORIES
 
-TEST_RETURN_CODE=$?
-if [ "${TEST_RETURN_CODE}" != "0" ]; then
-  echo "Gosec failed with return code $TEST_RETURN_CODE"
-  exit 1
+# Find all directories containing go.mod files
+if [ -n "$EXCLUDE_DIR" ]; then
+  EXCLUDED_PATHS=$(echo "$EXCLUDE_DIR" | sed 's/|/\/\*\" -not -path \"/g' | sed 's/^/-not -path \"/' | sed 's/$/\/\*\"/')
+  submodules=$(eval find . -name 'go.mod' "$EXCLUDED_PATHS" -exec dirname {} +)
+else
+  submodules=$(find . -name 'go.mod' -exec dirname {} +)
 fi
+
+for submodule in $submodules; do
+  echo "Running gosec on $submodule"
+  cd "$submodule"
+
+  echo "run gosec command: $(go env GOPATH)/bin/gosec -exclude-generated $EXCLUDE_FLAG $EXCLUDE_DIR_FLAG $DIRECTORIES"
+  $(go env GOPATH)/bin/gosec -exclude-generated $EXCLUDE_FLAG $EXCLUDE_DIR_FLAG $DIRECTORIES
+
+  TEST_RETURN_CODE=$?
+  if [ "${TEST_RETURN_CODE}" != "0" ]; then
+    echo "Gosec failed with return code $TEST_RETURN_CODE"
+    exit 1
+  fi
+
+  # Pop back to the parent directory
+  cd - > /dev/null
+done
 
 echo "Gosec ran successfully!"
 exit 0
