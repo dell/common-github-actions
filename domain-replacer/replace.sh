81937e4caa56f36851990b65f414b36df4fc459f
Alexander Hoppe <Alexander.Hoppe@dell.com>
2025-07-17 19:42:59 +0000

    initial commit

---
+++ domain-replacer/replace.sh
@@ -0,0 +1,12 @@
+set -e
+
+echo "Scanning for files to update..."
+
+
+find . -type f ! -name "go.sum" ! -path "./.github/workflows/*" | while read -r file; do
+  echo "Checking $file"
+  if grep -q 'github.com/dell/' "$file"; then
+    echo "Updating $file"
+    sed -i 's|github.com/dell/|eos2git.cec.lab.emc.com/CSM/|g' "$file"
+  fi
+done
