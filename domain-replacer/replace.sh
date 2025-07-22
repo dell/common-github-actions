4ec6eef10ab9db50c46f5af01120663b21fe1215
Alexander Hoppe <Alexander.Hoppe@dell.com>
2025-07-22 14:12:50 +0000

    latest changes

--- domain-replacer/replace.sh
+++ domain-replacer/replace.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
+# Copyright (c) 2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+
 set -e
 
 echo "Scanning for files to update..."
-
-# Ensure REPLACEMENT_DOMAIN is set
-if [ -z "$REPLACEMENT_DOMAIN" ]; then
-  echo "Error: REPLACEMENT_DOMAIN environment variable is not set."
-  exit 1
-fi
 
 find . -type f ! -name "go.sum" | while read -r file; do
   echo "Checking $file"
   if grep -q 'github.com/dell/' "$file"; then
     echo "Updating $file"
-    sed -i "s|github.com/dell/|$REPLACEMENT_DOMAIN|g" "$file"
+    sed -i 's|github.com/dell/|eos2git.cec.lab.emc.com/CSM/|g' "$file"
   fi
 done
